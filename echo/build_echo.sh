bd7b622f7a25964b11844c6e5ee557dab17254ef
Trevor a.k.a @rawkintrevo <trevor.d.grant@gmail.com>
2020-01-07 11:17:01 -0600

    echo stuff

--- echo/build_echo.sh
+++ echo/build_echo.sh
@@ -1,3 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
+image_name=rawkintrevo/kf-echo-test # Specify the image name here
+image_tag=latest
+full_image_name=${image_name}:${image_tag}
 
-docker build -t echo .
+cd "$(dirname "$0")"
+docker build -t "${full_image_name}" .
+docker push "$full_image_name"
+
+# Output the strict image name (which contains the sha256 image digest)
+# docker inspect --format="{{index .RepoDigests 0}}" "${IMAGE_NAME}"
