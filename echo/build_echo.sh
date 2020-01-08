b2348333fab3093476f0546643728ee98728ca46
Trevor a.k.a @rawkintrevo <trevor.d.grant@gmail.com>
2020-01-08 08:48:30 -0600

    local

--- echo/build_echo.sh
+++ echo/build_echo.sh
@@ -8,4 +8,4 @@ docker build -t "${full_image_name}" .
 docker push "$full_image_name"
 
 # Output the strict image name (which contains the sha256 image digest)
-# docker inspect --format="{{index .RepoDigests 0}}" "${IMAGE_NAME}"
+# docker inspect --format="{{index .RepoDigests 0}}" "${image_name}"
